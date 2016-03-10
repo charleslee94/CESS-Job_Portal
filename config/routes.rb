217fc75c0cbd9447998d0eeecb74a0802145a3f4
nateholmes3 <nateholmes3@berkeley.edu>
2016-03-10 22:05:35 +0000

    Trying to get index page up

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  resources :jobs
+  # map '/' to be a redirect to '/movies'
+  root :to => redirect('/jobs')
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
