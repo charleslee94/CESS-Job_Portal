a98bae7e881745ca3ef10009367b8ef4a858d44b
mtj1041 <mtjoseph@berkeley.edu>
2016-03-11 23:17:50 +0000

    Added beginner RSpec tests

--- spec/controllers/jobs_controller_spec.rb
+++ spec/controllers/jobs_controller_spec.rb
@@ -1,9 +1,12 @@
 require 'spec_helper'
+require 'rspec/rails'
+require 'rails_helper'
 
 describe JobsController do
   describe 'index should display all jobs' do
     it 'should call all jobs' do
-        
+        Job.should_receive(:all)
+        get :index
     end
   end
 end
