07efcb7fb68a4c7d804e3188b05c4b2c86de4c1f
mtj1041 <mtjoseph@berkeley.edu>
2016-03-15 22:18:04 +0000

    Added some more rspec tests

--- spec/controllers/jobs_controller_spec.rb
+++ spec/controllers/jobs_controller_spec.rb
@@ -14,14 +14,42 @@ describe JobsController do
   end
   
   describe 'rendering correct pages' do
+    it 'edit should call upon the correct id' do
+        #job = FactoryGirl.build(:job, :school => 'Matt', :title => 'Nate', :summary => 'Not Good', :expiration => '2016-10-22')
+        matt_job = jobs(:matt_job)
+        Job.should_receive(:find).with(matt_job.id.to_s)
+        get :edit, :id => matt_job.id
+    end
+    
+    it 'edit should render the edit page' do
+        #job = FactoryGirl.build(:job, :school => 'Matt', :title => 'Nate', :summary => 'Not Good', :expiration => '2016-10-22')
+        matt_job = jobs(:matt_job)
+        get :edit, :id => matt_job.id
+        response.should render_template("edit")
+    end
+    
     it 'edit should render the show page' do
         #job = FactoryGirl.build(:job, :school => 'Matt', :title => 'Nate', :summary => 'Not Good', :expiration => '2016-10-22')
         matt_job = jobs(:matt_job)
-        get :index, :id => matt_job.id
-        response.should render_template("index")
+        get :show, :id => matt_job.id
+        response.should render_template("show")
     end
   end
   
-  describe ''
+  describe 'load the correct variables into view' do
+    it 'should load correct job id into apply view' do
+      matt_job = jobs(:matt_job)
+      get :apply, :id => matt_job.id
+      actual_job = Job.find(matt_job.id)
+      assigns(:job).should == actual_job
+    end
+    
+    it 'should load correct job id into edit view' do
+      matt_job = jobs(:matt_job)
+      delete :destroy, :id => matt_job.id
+      actual_job = Job.find(matt_job.id)
+      assigns(:job).should == actual_job
+    end
+  end
   
 end
