c860b2c75f857a0470592ae17b662a87dcb89930
mtj1041 <mtjoseph@berkeley.edu>
2016-04-01 04:11:27 +0000

    added sad path rspec

--- spec/controllers/resumes_controller_spec.rb
+++ spec/controllers/resumes_controller_spec.rb
@@ -18,13 +18,24 @@ RSpec.describe ResumesController, type: :controller do
   end
   
   describe "POST #create" do
-    it "returns http success" do
+    it "create happy path" do
       attachment = File.new("#{Rails.root}/public/422.html")
       matt_job = jobs(:matt_job)
       resume = Resume.new({:name => 'Joseph, Fire', :attachment => attachment})
       Resume.should_receive(:new).and_return(resume)
       Job.should_receive(:find).and_return(@job)
       post :create, :jobid => @job.id, :resume => {:name => 'Joseph, Fire', :attachment => attachment}
+      flash[:notice].should =~ /has been uploaded/
+    end
+    
+    it "create sad path" do
+      attachment = File.new("#{Rails.root}/public/robots.txt")
+      matt_job = jobs(:matt_job)
+      resume = Resume.new({:name => 'Joseph, Fire', :attachment => attachment})
+      Resume.should_receive(:new).and_return(resume)
+      Job.should_receive(:find).and_return(@job)
+      post :create, :jobid => @job.id, :resume => {:name => 'Joseph, Fire', :attachment => attachment}
+      flash[:notice].should =~ /The upload failed/
     end
   end
 
