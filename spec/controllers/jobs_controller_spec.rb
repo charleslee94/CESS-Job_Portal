6407c49497c2ea5dc162af494fe533dde608ecd4
mtj1041 <mtjoseph@berkeley.edu>
2016-04-01 07:05:05 +0000

    improving coverage

--- spec/controllers/jobs_controller_spec.rb
+++ spec/controllers/jobs_controller_spec.rb
@@ -6,6 +6,10 @@ require 'factory_girl'
 describe JobsController do
   fixtures :jobs
   
+  before(:each) do
+    @job = jobs(:matt_job)
+  end
+  
   describe 'index should display all jobs' do
     it 'should call all jobs' do
         Job.should_receive(:all)
@@ -13,9 +17,19 @@ describe JobsController do
     end
   end
   
+  describe "POST #create" do
+    it "create happy path" do
+      Job.should_receive(:new).and_return(@job)
+      post :create, :job => {:school => @job.school, :title => @job.title, :summary => @job.summary, :compensation_min => @job.compensation_min, :compensation_max => @job.compensation_max, :expiration => @job.exipiration}
+      flash[:notice].should =~ /Job was successfully created/
+    end
+  
+  end
+  
   describe 'testing jobs#new' do
     it 'should call a new job' do
       get :new
+      expect(response).to have_http_status(:success)
     end
   end
   
@@ -23,22 +37,19 @@ describe JobsController do
   describe 'rendering correct pages' do
     it 'edit should call upon the correct id' do
         #job = FactoryGirl.build(:job, :school => 'Matt', :title => 'Nate', :summary => 'Not Good', :exipiration => '2016-10-22')
-        matt_job = jobs(:matt_job)
-        Job.should_receive(:find).with(matt_job.id.to_s)
-        get :edit, :id => matt_job.id
+        Job.should_receive(:find).with(@job.id.to_s)
+        get :edit, :id => @job.id
     end
     
     it 'edit should render the edit page' do
         #job = FactoryGirl.build(:job, :school => 'Matt', :title => 'Nate', :summary => 'Not Good', :exipiration => '2016-10-22')
-        matt_job = jobs(:matt_job)
-        get :edit, :id => matt_job.id
+        get :edit, :id => @job.id
         response.should render_template("edit")
     end
     
     it 'edit should render the show page' do
         #job = FactoryGirl.build(:job, :school => 'Matt', :title => 'Nate', :summary => 'Not Good', :exipiration => '2016-10-22')
-        matt_job = jobs(:matt_job)
-        get :show, :id => matt_job.id
+        get :show, :id => @job.id
         response.should render_template("show")
     end
   end
