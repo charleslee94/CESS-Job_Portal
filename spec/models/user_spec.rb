dbae2358e60e7a741155c3e41d7549aff9c157f2
mtj1041 <mtjoseph@berkeley.edu>
2016-04-01 19:06:51 +0000

    user rspec test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'user should be created successfully' do
+    it 'should call all jobs' do
+        @user = User.create!({:email => 'l8@l8.com', :password => 'l8l8l8l8l8l8', :encrypted_password => 'lame'})
+        @user.email.should == 'l8@l8.com'
+    end
+  end
 end
