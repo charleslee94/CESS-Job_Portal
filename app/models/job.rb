798459ab945407aa834b03e5e8a6f12464077ee6
Noah Keen <noahkeen@berkeley.edu>
2016-04-12 06:14:37 +0000

    validation fix

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,13 +1,13 @@
 class Job < ActiveRecord::Base
     has_many :resumes
-    validates :job_not_expired
-    validates :expiration, presence: true
-    validates :school, presence: true
-end
+    validate :job_not_expired
+    validates :expiration, :school, presence: true
 
-def job_not_expired
-   if expiration and expiration.past?
-       errors.add(:base, "This job has expired.")
-       #self.delete!
-   end
+
+    def job_not_expired
+       if expiration and expiration.past?
+           errors.add(:base, "This job has expired.")
+           #self.delete!
+       end
+    end
 end
