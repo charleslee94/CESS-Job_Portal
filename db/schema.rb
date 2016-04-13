c6507c69cb5a8af7b6a30f9aa793d6634fa54ee6
jannaerin <jannagolden@berkeley.edu>
2016-04-13 22:11:13 +0000

    fixed col name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160413074521) do
+ActiveRecord::Schema.define(version: 20160413220147) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "school"
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 20160413074521) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                                   null: false
     t.datetime "updated_at",                                   null: false
-    t.string   "type",                   default: "candidate"
+    t.string   "user_type",              default: "candidate"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
