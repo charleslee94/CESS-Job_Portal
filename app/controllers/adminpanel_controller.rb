eac51a565ba8a4a7dc1173eea874f53aa931eacf
mtj1041 <mtjoseph@berkeley.edu>
2016-04-14 06:09:34 +0000

    Added stuff

--- app/controllers/adminpanel_controller.rb
+++ app/controllers/adminpanel_controller.rb
@@ -1,7 +1,11 @@
 class AdminpanelController < ApplicationController
     def index
-        if not current_user.user_type == 'admin'
-            flash[:notice] = 'You are not authorized to view this page'
+        if current_user
+            if not current_user.user_type == 'admin'
+                flash[:notice] = 'You are not authorized to view this page'
+                redirect_to '/jobs'
+            end
+        else
             redirect_to '/jobs'
         end
     end
@@ -11,6 +15,8 @@ class AdminpanelController < ApplicationController
     end
     
     def create_new_school
+      user_hash = {:email => params[:email], :password => params[:password], :user_type => 'school'}
+      School.create!(user_hash)
       redirect_to '/admin'
     end
 
