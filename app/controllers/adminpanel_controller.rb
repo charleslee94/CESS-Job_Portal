29196058e0e58c770dbf95d6122bd88f8e9a30ba
Charles Lee <charleslee94@berkeley.edu>
2016-04-13 23:05:14 -0700

    school creation properly creates schools

--- app/controllers/adminpanel_controller.rb
+++ app/controllers/adminpanel_controller.rb
@@ -11,6 +11,7 @@ class AdminpanelController < ApplicationController
     end
     
     def create_new_school
+      @user = User.create(email: params[:user][:email], school: params[:user][:school], password: params[:user][:password])
       redirect_to '/admin'
     end
 
