4ed71ae765ea5ac4c512c21627cf2fe875b32df1
Sophie Cooper <sophie@cooperzone.net>
2016-04-15 23:34:22 +0000

    saved sorting preferences

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -12,7 +12,12 @@ class JobsController < ApplicationController
     end
     if sort
       @jobs = Job.order(sort + ' ASC')
-    else
+      session[:sort] = sort
+      print "IN SORT !!!!"
+    elsif (session[:sort] != nil)
+      sort = session[:sort]  
+      @jobs = Job.order(sort + ' ASC')
+    else 
       @jobs = Job.all
     end
   end
