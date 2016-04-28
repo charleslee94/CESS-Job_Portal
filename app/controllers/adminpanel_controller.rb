1fa493187856173fe783e43291ef3cd3e6a2a84a
jannaerin <jannagolden@berkeley.edu>
2016-04-28 19:56:00 +0000

    8 character password flash notice

--- app/controllers/adminpanel_controller.rb
+++ app/controllers/adminpanel_controller.rb
@@ -19,14 +19,20 @@ class AdminpanelController < ApplicationController
     
     
     def create_new_school
-      @user = User.new(email: params[:user][:email], school: params[:user][:school], password: params[:user][:password], user_type: 'school')
-      if @user.save 
+      if params[:user][:password].length >= 8
+        @user = User.new(email: params[:user][:email], school: params[:user][:school], password: params[:user][:password], user_type: 'school')
+        if @user.save 
           flash[:notice] = 'You have successfully created a new school. Log in to your school account to start posting job listings.'
           redirect_to '/jobs'
-      else
+        else
           flash[:notice] = 'Something went wrong in the creation of this user.'
           redirect_to '/admin'
+        end
+      else
+        flash[:notice] = 'Account not created! Password must be at least 8 characters in length.'
+        redirect_to '/admin/newschool'
       end
+    
     end
     
     
